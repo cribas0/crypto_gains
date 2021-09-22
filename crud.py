edcf04cbed2ce7fee20385691f2a3c4fc3c96f6d
Minho16 <minho1693@gmail.com>
2021-09-22 23:04:44 +0200

    buy & sell transaction -> transaction + bool

--- crud.py
+++ crud.py
@@ -26,17 +26,12 @@ def add_user_db(lastname, firstname, password, email):
     my_cursor.execute(add_user, user_info)
     mydb.commit()
 
-def add_buy_tx_db(userid, buy_time, buy_base_currency, buy_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, buy_price):
-    add_buy_tx = "INSERT INTO buy_tx (userid, buy_time, buy_base_currency, buy_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, buy_price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    buy_tx_info = (userid, buy_time, buy_base_currency, buy_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, buy_price)
-    my_cursor.execute(add_buy_tx, buy_tx_info)
+def add_transaction_db(userid, trade_time, money_currency, coin_currency, initial_money_amount, final_money_amount, initial_coin_amount, final_coin_amount, money_coin_rate, buy_boolean):
+    add_transaction = "INSERT INTO transaction (userid, trade_time, money_currency, coin_currency, initial_money_amount, final_money_amount, initial_coin_amount, final_coin_amount, money_coin_rate, buy_boolean) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    transaction_info = (userid, trade_time, money_currency, coin_currency, initial_money_amount, final_money_amount, initial_coin_amount, final_coin_amount, money_coin_rate, buy_boolean)
+    my_cursor.execute(add_transaction, transaction_info)
     mydb.commit()
 
-def add_sell_tx_db(userid, sell_time, sell_base_currency, sell_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, sell_price):
-    add_sell_tx = "INSERT INTO sell_tx (userid, sell_time, sell_base_currency, sell_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, sell_price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    sell_tx_info = (userid, sell_time, sell_base_currency, sell_coin_currency, initial_balance, final_balance, initial_coin_amount, final_coin_amount, sell_price)
-    my_cursor.execute(add_sell_tx, sell_tx_info)
-    mydb.commit()
 
 def add_user_balance_db(userid, amount, currency):
     add_user_balance = "INSERT INTO user_balance (userid, amount, currency) VALUES (%s, %s, %s)"
@@ -44,6 +39,7 @@ def add_user_balance_db(userid, amount, currency):
     my_cursor.execute(add_user_balance, user_balance_info)
     mydb.commit()
 
+#add_transaction_db(2, "2021-09-16 03:07:00", "USD", "BTC", 1000, 200, 0, 10, 80,True)
 #add_user_db("Jang","Minho", "jijiji", "minho1693@gmail.com")
 #add_buy_tx_db(3, "2021-09-16 03:07:00", "USD", "BTC", 1000, 200, 0, 10, 80)
 #add_sell_tx_db(3, "2021-09-16 03:07:00", "BTC", "USD", 200, 1000, 10, 0, 80)
@@ -65,44 +61,29 @@ def get_user_db(id):
         print("Email  = ", column[4]) # UNIQUE
 
 
-get_user_db(1)
+#get_user_db(1)
 
-def get_buy_tx_db(id):
-    get_buy_tx = "SELECT * FROM buy_tx WHERE id = {0}".format(id)
-    my_cursor.execute(get_buy_tx)
+def get_transaction_db(id):
+    get_transaction = "SELECT * FROM transaction WHERE id = {0}".format(id)
+    my_cursor.execute(get_transaction)
     get_transaction_info = my_cursor.fetchall()
 
     print("\nPrinting each column of the id {0}".format(id))
     for column in get_transaction_info:
         print("ID = ", column[0])
         print("UserID = ", column[1])
-        print("Buy Time  = ", column[2])
-        print("Buy Base Currency  = ", column[3])
-        print("Buy Coin Currency  = ", column[4])
+        print("Trade Time  = ", column[2])
+        print("Money Currency  = ", column[3])
+        print("Coin Currency  = ", column[4])
         print("Initial Balance (EUR)  = ", column[5])
         print("Final Balance (EUR)  = ", column[6])
         print("Initial Coin amount (EA)  = ", column[7])
         print("Final Coin amount (EA)  = ", column[8])        
-        print("Buy Price (EUR/EA)  = ", column[9])
+        print("Money Coin Rate (EUR/EA)  = ", column[9])
+        print("Purchasing action TRUE or NOT  = ", column[10])
 
 
-def get_sell_tx_db(id):
-    get_sell_tx = "SELECT * FROM sell_tx WHERE id = {0}".format(id)
-    my_cursor.execute(get_sell_tx)
-    get_transaction_info = my_cursor.fetchall()
-
-    print("\nPrinting each column of the id {0}".format(id))
-    for column in get_transaction_info:
-        print("ID = ", column[0])
-        print("UserID = ", column[1])
-        print("Sell Time  = ", column[2])
-        print("Sell Base Currency  = ", column[3])
-        print("Sell Coin Currency  = ", column[4])
-        print("Initial Balance (EUR)  = ", column[5])
-        print("Final Balance (EUR)  = ", column[6])
-        print("Initial Coin amount (EA)  = ", column[7])
-        print("Final Coin amount (EA)  = ", column[8])        
-        print("Sell Price (EUR/EA)  = ", column[9])
+#get_transaction_db(1)
 
 def get_user_balance_db(id):
     get_user_balance = "SELECT * FROM user_balance WHERE userid = {0}".format(id)
@@ -127,17 +108,13 @@ def update_user_db(column_name, item, id):
     my_cursor.execute(update_user,update_user_info)
     mydb.commit()
 
-def update_buy_tx_db(column_name, item, id):
-    update_buy_tx ="""UPDATE buy_tx SET `{0}` = %s WHERE id = %s""".format(column_name) # <- SQL columns inside ` ` and solve it wz format
-    update_buy_tx_info = (item, id)    
-    my_cursor.execute(update_buy_tx,update_buy_tx_info)
+def update_transaction_db(column_name, item, id):
+    update_transaction ="""UPDATE transaction SET `{0}` = %s WHERE id = %s""".format(column_name) # <- SQL columns inside ` ` and solve it wz format
+    update_transaction_info = (item, id)    
+    my_cursor.execute(update_transaction,update_transaction_info)
     mydb.commit()
 
-def update_sell_tx_db(column_name, item, id):
-    update_sell_tx ="""UPDATE sell_tx SET `{0}` = %s WHERE id = %s""".format(column_name) # <- SQL columns inside ` ` and solve it wz format
-    update_sell_tx_info = (item, id)    
-    my_cursor.execute(update_sell_tx,update_sell_tx_info)
-    mydb.commit()
+#update_transaction_db('money_currency', 'EUR', 1)
 
 # Here, both id and currency MUST match in order to change the value correctly (currency ==> UNIQUE)
 def update_user_balance_db(column_name, item, id, currency):
@@ -146,7 +123,6 @@ def update_user_balance_db(column_name, item, id, currency):
     my_cursor.execute(update_user,update_user_info)
     mydb.commit()    
 
-#update_sell_tx_db('sell_base_currency', 'EUR', 1)
 #update_user_balance_db("amount", 100, 1, "BTC")
 
 
@@ -157,21 +133,15 @@ def delete_user_db(id):
     my_cursor.execute(delete_user)
     mydb.commit()
 
-def delete_buy_tx_db(id): 
-    delete_buy_tx = "DELETE FROM buy_tx WHERE id = " + str(id)
-    my_cursor.execute(delete_buy_tx)
+def delete_transaction_db(id):
+    delete_transaction = "DELETE FROM transaction WHERE id = " + str(id)
+    my_cursor.execute(delete_transaction)
     mydb.commit()
-
-def delete_sell_tx_db(id): 
-    delete_sell_tx = "DELETE FROM sell_tx WHERE id = " + str(id)
-    my_cursor.execute(delete_sell_tx)
-    mydb.commit()
-
 
 def delete_user_balance_db(id): 
     delete_user_balance = "DELETE FROM user_balance WHERE id = " + str(id)
     my_cursor.execute(delete_user_balance)
     mydb.commit()
 
+#delete_transaction_db(2)
 #delete_user_db(4)
-#delete_buy_tx_db(1)
