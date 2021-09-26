82f2ac9d4768f5f60bd9b67178f678f12ce8e995
Minho16 <minho1693@gmail.com>
2021-09-26 02:58:02 +0200

    add_transaction_db function

--- crud.py
+++ crud.py
@@ -8,7 +8,7 @@ from coin_base import get_buy_price, get_sell_price
 
 now = datetime.now()
 date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-print(date_time)
+#print(date_time)
 
 with open('secret/credentials.json') as json_file:
     data = json.load(json_file)
@@ -40,16 +40,25 @@ def add_user_db(UserSignUp: UserSignUp) -> None:
 def add_transaction_db(AddTransaction: AddTransaction) -> None:
     add_transaction = "INSERT INTO transaction (userid, trade_time, money_currency, coin_currency, initial_money_amount, final_money_amount, initial_coin_amount, final_coin_amount, money_coin_rate, IsBuy) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     
-    if AddTransaction.IsBuy == True:
-        money_coin_rate = get_buy_price(AddTransaction.coin_currency,AddTransaction.money_currency)
-    else: 
-        money_coin_rate = get_sell_price(AddTransaction.coin_currency,AddTransaction.money_currency)
+    initial_money_amount = float(get_user_balance_byCurrency_db(AddTransaction.userid, AddTransaction.money_currency)) 
+    initial_coin_amount = float(get_user_balance_byCurrency_db(AddTransaction.userid, AddTransaction.coin_currency))
 
-    transaction_info = (AddTransaction.userid, date_time, AddTransaction.money_currency, AddTransaction.coin_currency, AddTransaction.initial_money_amount, AddTransaction.final_money_amount, AddTransaction.initial_coin_amount, AddTransaction.final_coin_amount, money_coin_rate, AddTransaction.IsBuy)
+    if AddTransaction.IsBuy == True:
+        money_coin_rate = float(get_buy_price(AddTransaction.coin_currency,AddTransaction.money_currency))
+        final_money_amount = initial_money_amount - AddTransaction.traded_money_amount
+        final_coin_amount = initial_coin_amount + AddTransaction.traded_money_amount/money_coin_rate
+        update_user_balance_db("amount",final_money_amount,AddTransaction.userid,AddTransaction.money_currency)
+        update_user_balance_db("amount",final_coin_amount,AddTransaction.userid,AddTransaction.coin_currency)
+    else: 
+        money_coin_rate = float(get_sell_price(AddTransaction.coin_currency,AddTransaction.money_currency))
+        final_coin_amount = initial_coin_amount - AddTransaction.traded_coin_amount
+        final_money_amount = initial_money_amount + AddTransaction.traded_coin_amount*money_coin_rate
+        update_user_balance_db("amount",final_money_amount,AddTransaction.userid,AddTransaction.money_currency)
+        update_user_balance_db("amount",final_coin_amount,AddTransaction.userid,AddTransaction.coin_currency)
+
+    transaction_info = (AddTransaction.userid, date_time, AddTransaction.money_currency, AddTransaction.coin_currency, initial_money_amount, final_money_amount, initial_coin_amount, final_coin_amount, money_coin_rate, AddTransaction.IsBuy)
     my_cursor.execute(add_transaction, transaction_info)
     mydb.commit()
-
-
 
 def add_user_balance_db(userid, amount, currency):
     add_user_balance = "INSERT INTO user_balance (userid, amount, currency) VALUES (%s, %s, %s)"
@@ -67,14 +76,14 @@ def get_user_db(id):
     get_user = "SELECT * FROM users WHERE id = {0}".format(id)
     my_cursor.execute(get_user)
     get_user_info = my_cursor.fetchall()
-
+    
     print("\nPrinting each column of the id {0}".format(id))
     for column in get_user_info:
         print("ID = ", column[0], ) # AUTO INCREMENT & PRIMARY KEY
         print("LastName = ", column[1])
         print("FirstName  = ", column[2])
         print("Password  = ", column[3])
-        print("Email  = ", column[4]) # UNIQUE
+        print("Email  = ", column[4]) # UNIQUE 
 
 #get_user_db(1)
 
@@ -97,19 +106,29 @@ def get_transaction_db(id):
         print("Money Coin Rate (EUR/EA)  = ", column[9])
         print("Purchasing action TRUE(1) or NOT(0)  = ", column[10])
 
+#get_transaction_db(1)
+
 def get_user_balance_db(id):
     get_user_balance = "SELECT * FROM user_balance WHERE userid = {0}".format(id)
     my_cursor.execute(get_user_balance)
-    get_user_info = my_cursor.fetchall()
-
+    get_user_balance_info = my_cursor.fetchall()
     print("\nPrinting each column of the userid {0}".format(id))
-    for column in get_user_info:
+    
+    for column in get_user_balance_info:
         print("User ID = ", column[0], )
         print("Amount (EUR) = ", column[1])
         print("Currency  = ", column[2])
 
-#get_transaction_db(1)
 #get_user_balance_db(1)
+
+
+def get_user_balance_byCurrency_db(id,currency):
+    get_user_balance_byCurrency = 'SELECT * FROM user_balance WHERE userid = {0} AND currency = "{1}"'.format(id,currency)
+    my_cursor.execute(get_user_balance_byCurrency)
+    get_user_balance_info = my_cursor.fetchall()    
+    for x in get_user_balance_info:
+        return x[1]
+
 
 ## EDIT USER/TRANSACTION FUNCTION BY ID, COLUMN_NAME AND ITEM
 
@@ -134,7 +153,9 @@ def update_user_balance_db(column_name, item, id, currency):
     my_cursor.execute(update_user,update_user_info)
     mydb.commit()    
 
-#update_user_balance_db("amount", 100, 1, "BTC")
+
+
+#update_user_balance_db("amount", 10000, 1, "EUR")
 
 
 ## DELETE USER/TRANSACTION BY ID 
