f9f7eb2db3cb6df683dea5661e0729804c1befd0
Minho16 <minho1693@gmail.com>
2021-10-06 02:10:04 +0200

    Simple HTTP error response for each API

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, status
 from requests.sessions import Request
-from crud import (add_user_db,add_transaction_db, delete_transaction_db, delete_user_balance_db,get_user_db,get_user_balance_db,delete_user_db,add_user_balance_db, delete_user_balance_db, delete_transaction_db)
+from crud import (add_user_db,add_transaction_db, delete_transaction_db, delete_user_balance_db, get_transaction_db,get_user_db,get_user_balance_db,delete_user_db,add_user_balance_db, delete_user_balance_db, delete_transaction_db)
 from coin_base import (get_buy_price, get_sell_price)
 from pydantic_models import (UserSignUp, AddTransaction, StandardResponse, UserMoneyCharge
 )
@@ -15,7 +15,7 @@ def SignUpUser(RequestBody: UserSignUp):
         raise HTTPException (
             status_code = status.HTTP_409_CONFLICT,
             detail = (
-                ("The user already exists")
+                ("The email address already exists")
             )
         ) 
     return StandardResponse(code = "Success", message = "User created successfully")
@@ -23,16 +23,29 @@ def SignUpUser(RequestBody: UserSignUp):
 
 @app.post("/add_transaction")
 def addTransaction(RequestBody: AddTransaction): 
-#    try:
+    try:
         add_transaction_db(RequestBody)
-
-#    except:
-#        return ("Error")
-#    return StandardResponse(code = "Success", message = "User created successfully")
+    except:
+        raise HTTPException (
+            status_code= status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail = (
+                ("Please check again the operation")
+            )
+        )
+    return StandardResponse(code = "Success", message = "Transaction done successfully")
 
 @app.post("/credit_charge")
 def addUserBalanceDB(RequestBody: UserMoneyCharge):
-    add_user_balance_db(RequestBody)
+    try:
+        add_user_balance_db(RequestBody)
+    except:
+            raise HTTPException (
+                status_code= status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail = (
+                    ("Please check again the input data")
+                )
+            )
+    return StandardResponse(code = "Success", message = "Credit charge done successfully")
 
 
 @app.get("/get_buy_price/{coin_currency}/{base_currency}")
@@ -48,10 +61,14 @@ def GetSellPrice(coin_currency: str, base_currency: str):
 def GetUserInfo(user_id: int):
     return(get_user_db(user_id))
 
+@app.get("/get_user_transaction_by_id/{user_id}")
+def GetUserTransactionByID(user_id: int):
+    return(get_transaction_db(user_id))
+
+
 @app.get("/get_user_balance_by_id/{user_id}")
 def GetUserBalanceInfo(user_id: int):
     return(get_user_balance_db(user_id))
-
 
 @app.delete("/delete_user_by_id/{user_id}")
 def DeleteUserInfo(user_id: int):
